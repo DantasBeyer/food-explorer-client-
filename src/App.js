db376ef2228b6be83bb11c86b28c24e20af539f0
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-24 17:53:18 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Footer from './components/Footer/footer';
+import Button from './components/Button/button';
 import './style/input.css'
+
 
 function App() {
   return (
     <div className="App">
       <Footer/>
+      <Button/>
       
     </div>
   );
---
+++ src/components/Button/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./button.css"
+
+function  button() {
+
+  return (
+    <div>
+        <button id='btn_1' className='btn'>include</button>
+    </div>
+  )
+}
+
+export default  button
