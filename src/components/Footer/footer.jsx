6327956c21d64af4d88b9c174845787dd89363f5
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-24 16:24:44 +0200

    updated

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './footer.css'
+import DishesImgParma from '../../img/dishes/parma.png'
 
 function footer() {
   return (
+    
     <div className='footer'>
         <p>home</p>
         <p>tutorials</p>
         <p>about</p>
         <p>contact</p>
+        <img src={DishesImgParma} alt="parma" />
     </div>
+    
   )
 }
 
