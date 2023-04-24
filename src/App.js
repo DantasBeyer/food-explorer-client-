d73eae17857aba927af7dca80dc35aea82ff8d33
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-24 10:13:59 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Footer from './components/Footer/footer';
+import './style/input.css'
 
 function App() {
   return (
     <div className="App">
-    Food App      
+      <Footer/>
+      
     </div>
   );
 }
---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+function footer() {
+  return (
+    <div className='footer'>
+        <h2>home</h2>
+        <h2>tutorials</h2>
+        <h2>about</h2>
+        <h2>contact</h2>
+    </div>
+  )
+}
+
+export default footer
