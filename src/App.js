b52734cffa99452429eea4fe949098cf28e0ca0c
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-27 13:09:20 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './style/input.css'
 import Button from './components/Button/button';
+import ButtonAdm from './components/Button/buttonAdm';
 import Tag from './components/Tag/tag'
 import Stepper from './components/Stepper/stepper';
 import Myorders from './components/MyOrders/myorders.jsx'
 import Navbar from './components/Navbar/navbar';
+import NavbarAdm from './components/Navbar/navbarAdm';
 
 function App() {
   return (
     <div className="App">
      
       <Navbar/>
+      <NavbarAdm/>
       <br/>
       <Button/>
+      <ButtonAdm/>
       <br/>
       <Tag/>
       <Stepper/> 
---
+++ src/components/Button/buttonAdm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './button.css'
+
+function buttonAdm() {
+  return (
+    <div className='btn_container_adm'>
+        New Dishes
+    </div>
+  )
+}
+
+export default buttonAdm
