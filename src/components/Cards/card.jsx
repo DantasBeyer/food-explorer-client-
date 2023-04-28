7b9ebcd74687b061e5f4b3eb3a108d3ffe3ac16a
Dantas Beyer <dantasbeyer@gotmail.com>
2023-04-28 23:05:33 +0200

    card One created

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import './card.css'
+import dishImg from '../../img/dishes/parma.png'
+import heartImg from '../../img/icons/heart.png'
+import heartClickedImg from '../../img/icons/heart_clicked.png'
+import Stepper from '../Stepper/stepper'
+import Button from '../Button/button'
+
+
 
 function card() {
   return (
     <div className='card_container'>
 
-    first card
+      <div className='heart'>
+        <img id='heart' src={heartImg} alt="hear_img" />
+      </div>
 
-    criar um card so de cada modalidade, 1 adm, 1 normal e para mobile 
-    os demais serao feitos em server-side (MERN)
+      <div className='dish'>
+        <img id='cardImg' src={dishImg} alt="dish_img" />
+      </div>
+
+      <div className='title'>
+        <p>
+          Torradas de Parma >
+        </p>
+      </div>
+
+      <div className='sub_title'>
+        <p>
+          Presunto de parma e rúcula em um pão com fermentação natural.
+        </p>
+      </div>
+      
+      <div className='price'>
+        <p>
+          € 7,00
+        </p>
+      </div>
+
+      <div className='interaction'>
+        <Stepper/>
+        <Button/>
+      </div>
+      
+
     </div>
   )
 }
