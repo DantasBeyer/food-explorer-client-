fecd5e03ef8f6998e72dcac74a237c872df7e400
Dantas Beyer <dantasbeyer@gotmail.com>
2023-05-03 16:18:49 +0200

    updated

--- src/components/Cards/card_adm.jsx
+++ src/components/Cards/card_adm.jsx
@@ -2,38 +2,33 @@ import React from 'react'
 import './card.css'
 import dishImg from '../../img/dishes/parma.png'
 import pencil from '../../img/icons/pencil.png'
-import heartClickedImg from '../../img/icons/heart_clicked.png'
-import Stepper from '../Stepper/stepper'
-import Button from '../Button/button'
-
-
 
 function card_adm() { 
   return (
     
-    <div className='card_container'>
+    <div className='card_container_adm'>
 
-    <div className='heart'>
-        <img id='heart' src={pencil} alt="hear_img" />
+    <div className='pencil_adm'>
+        <img id='pencil' src={pencil} alt="pencil_img" />
     </div>
 
-    <div className='dish'>
+    <div className='dish_adm'>
         <img id='cardImg' src={dishImg} alt="dish_img" />
     </div>
 
-    <div className='title'>
+    <div className='title_adm'>
         <p>
         Torradas de Parma 
         </p>
     </div>
 
-    <div className='sub_title'>
+    <div className='sub_title_adm'>
         <p>
-        Presunto de parma e rúcula em um pão com fermentação natural.
+          Presunto de parma e rúcula em um pão com fermentação natural.
         </p>
-    </div>
+      </div>
     
-    <div className='price'>
+    <div className='price_adm'>
         <p>
         € 7,00
         </p>
