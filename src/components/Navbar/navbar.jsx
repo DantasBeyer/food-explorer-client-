38767739c82e9ede3465d853c902850b1fd75493
Dantas Beyer <dantasbeyer@gotmail.com>
2023-05-11 09:28:54 +0200

    updated

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Myorders from "../MyOrders/Myorders";
 import exitImg from "../../img/icons/logout.png";
 import Logo from "../Logo/Logo";
-import hamburgerImg from "../../img/icons/hamburger.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
-function Navbar() {
+
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+ 
+
   return (
     <div id="navbar_container">
-      <img className="hamburgerImg" src={hamburgerImg} alt="hamburger_img" />
+      <div className="myorders">
+        <Myorders />
+      </div>
+
       <Logo />
       <input
         className="search_bar"
         type="text"
         placeholder='"img" search dishes or ingredients'
       />
-      <div className="myorders">
-        <Myorders />
+      <div id="hamburger" onClick={handleClick} className="hamburger">
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
+
+      <ul className="menu_hidden"></ul>
       <img className="exitImg" src={exitImg} alt="logout_img" />
     </div>
   );
-}
+};
 
 export default Navbar;
