c945739208a5df257528c3dc3340226b0d21b8f7
Dantas Beyer <dantasbeyer@gotmail.com>
2023-05-17 18:39:27 +0200

    updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import "./home.css";
 function Home() {
   return (
     <div className="home_container">
+    
       <SliderMeals />
       <SliderDesserts />
       <SliderDrinks />
---
+++ src/components/AuthenticationComp/SignIn.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../../style/tailwind.css";
+import Logo from "../Logo/Logo";
+
+function SignIn({ onClick }) {
+  return (
+    <div className="auth_container">
+      <header>
+        <p className="text-gray-300 text-2xl text-center mb-6 ">Sign in</p>
+        {/* <Logo className="hidden"/> */}
+      </header>
+
+      <section className="inputs">
+        <p className="text-gray-400 text-xs">Email</p>
+        <input className=" text-white bg-slate-800 rounded mb-3" type="text" />
+        <p className="text-gray-400 text-xs">Password</p>
+        <input className=" text-white bg-slate-800 rounded" type="text" />
+      </section>
+      <footer className="flex flex-col  text-center">
+        <button
+          href="/"
+          className="text-gray-100 text-sm bg-red-900 hover:bg-red-800  active:bg-red-700 rounded h-7 mt-6">
+          Enter
+        </button>
+        <a onClick={onClick} className="text-gray-200 text-xs mt-3 cursor-pointer">
+          
+          Create an account
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default SignIn;
