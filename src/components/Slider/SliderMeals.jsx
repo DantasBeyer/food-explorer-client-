19339ae6454d5b448f2e4c1d6f26d623dd78ee88
Dantas Beyer <dantasbeyer@gotmail.com>
2023-05-23 08:15:42 +0200

    updated

--- src/components/Slider/SliderMeals.jsx
+++ src/components/Slider/SliderMeals.jsx
@@ -3,68 +3,50 @@ import "./Slider.css";
 import CardParma from "../Cards/meals/CardParma";
 import CardSalad from "../Cards/meals/CardSalad";
 import CardGambe from "../Cards/meals/CardGambe";
-import React, { useRef, useState } from 'react';
-import SwiperCore, { Virtual, Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef, useState } from "react";
+import SwiperCore, { Virtual, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 SwiperCore.use([Virtual, Navigation, Pagination]);
 
-
 function Slider() {
-  const [swiperRef, setSwiperRef] = useState(null);
-  const appendNumber = useRef(500);
-  const prependNumber = useRef(1);
-  // Create array with 500 slides
-  const [slides, setSlides] = useState(
-    Array.from({ length: 500 }).map((_, index) => `Slide ${index + 1}`)
-  );
-
-  const prepend = () => {
-    setSlides([
-      `Slide ${prependNumber.current - 2}`,
-      `Slide ${prependNumber.current - 1}`,
-      ...slides,
-    ]);
-    prependNumber.current = prependNumber.current - 2;
-    swiperRef.slideTo(swiperRef.activeIndex + 2, 0);
-  };
-
-  const append = () => {
-    setSlides([...slides, 'Slide ' + ++appendNumber.current]);
-  };
-
-  const slideTo = (index) => {
-    swiperRef.slideTo(index - 1, 0);
-  };
-
   return (
     <div className="slider_container">
-    <Swiper
-    spaceBetween={80}
-    slidesPerView={3}
-
-    onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}
-    pagination={{
-      type: 'fraction',
-    }}
-    navigation={true}
-    virtual
-  >
-    <SwiperSlide><CardParma/></SwiperSlide>
-    <SwiperSlide><CardSalad/></SwiperSlide>
-    <SwiperSlide><CardGambe/></SwiperSlide>
-    <SwiperSlide><CardParma/></SwiperSlide>
-    <SwiperSlide><CardSalad/></SwiperSlide>
-    <SwiperSlide><CardGambe/></SwiperSlide>
-    ...
-  </Swiper>
-
+      <Swiper
+        spaceBetween={80}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        pagination={{
+          type: "fraction",
+        }}
+        navigation={true}
+        virtual>
+        <SwiperSlide>
+          <CardParma />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardSalad />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardGambe />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardParma />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardSalad />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardGambe />
+        </SwiperSlide>
+        ...
+      </Swiper>
     </div>
   );
 }
