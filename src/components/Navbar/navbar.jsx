555c5c6b7f58d85ea57ee9cded206a6405ceff41
Dantas Beyer <dantasbeyer@gotmail.com>
2023-05-24 14:32:33 +0200

    hamburger working

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -25,37 +25,30 @@ const Navbar = () => {
       <a href="/auth">
         <img className="exitImg" src={exitImg} alt="logout_img" />
       </a>
-      <div onClick={handleClick} className="md:hidden z-10 ">
-        {!nav ? <FaBars className=" w-8 h-8 bg-transparent"/> : <FaTimes className=" w-8 h-8 bg-transparent" />}
+      <div onClick={handleClick} className="md:hidden z-20 bg-transparent">
+        {!nav ? (
+          <FaBars className=" w-8 h-8 bg-transparent" />
+        ) : (
+          <FaTimes className=" w-8 h-8 bg-transparent" />
+        )}
       </div>
 
       <ul
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#000000] flex flex-col justify-center items-center"
+            : "absolute top-0 left-0 w-full h-screen bg-[#20202e] flex flex-col justify-center items-center z-10"
         }>
-        <li
-          onClick={handleClick}
-          to="home" >
-          <h1 className="text-white pt-0">Home</h1>
+        <li onClick={handleClick} to="home">
+          <h1 className="text-white bg-[#20202e]">Home</h1>
         </li>
-        <li
-        onClick={handleClick}
-        to="exit">
-        <h1 className="text-white">Exit</h1>
-      </li>
+        <li onClick={handleClick} to="exit">
+          <h1 className="text-white bg-[#20202e]">Exit</h1>
+        </li>
+        <li>
+          <Menu />
+        </li>
       </ul>
-
-      {/*   <div id="hamburger" onClick={handleClick}>
-        {nav ? (
-          <FaBars className="bars" />
-        ) : (
-          <div>
-            <Menu />
-          </div>
-        )}
-      </div> */}
     </div>
   );
 };
