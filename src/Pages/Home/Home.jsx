6c83c2429900b9731d5176c47548e3bee9e9e43c
Dantas Beyer <dantasbeyer@gotmail.com>
2023-05-24 07:32:36 +0200

    updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import SliderDesserts from "../../components/Slider/SliderDesserts";
 import SliderMeals from "../../components/Slider/SliderMeals";
 import SliderDrinks from "../../components/Slider/SliderDrinks";
+import headerImg from "../../img/icons/macaronsHome.png";
 import "./home.css";
+import "../../style/tailwind.css";
 
 function Home() {
   return (
     <div className="home_container">
-    
-    <SliderMeals />
-    <SliderDesserts />
-    <SliderDrinks /> 
-     
+      <header className="header">
+        <img src={headerImg} alt="header_img" />
 
+        <div className="slogam">
+          <p>Sabores inigualáveis</p>
+          <p>Sinta o cuidado do preparo com ingedientes selecionados</p>
+        </div>
+      </header>
+
+      <SliderMeals />
+      <SliderDesserts />
+      <SliderDrinks />
     </div>
   );
 }
--- src/components/Slider/SliderDrinks.jsx
+++ src/components/Slider/SliderDrinks.jsx
@@ -12,48 +12,19 @@ import 'swiper/css/navigation';
 
 SwiperCore.use([Virtual, Navigation, Pagination]);
 
-
 function Slider() {
-  const [swiperRef, setSwiperRef] = useState(null);
-  const appendNumber = useRef(500);
-  const prependNumber = useRef(1);
-  // Create array with 500 slides
-  const [slides, setSlides] = useState(
-    Array.from({ length: 500 }).map((_, index) => `Slide ${index + 1}`)
-  );
-
-  const prepend = () => {
-    setSlides([
-      `Slide ${prependNumber.current - 2}`,
-      `Slide ${prependNumber.current - 1}`,
-      ...slides,
-    ]);
-    prependNumber.current = prependNumber.current - 2;
-    swiperRef.slideTo(swiperRef.activeIndex + 2, 0);
-  };
-
-  const append = () => {
-    setSlides([...slides, 'Slide ' + ++appendNumber.current]);
-  };
-
-  const slideTo = (index) => {
-    swiperRef.slideTo(index - 1, 0);
-  };
-
   return (
     <div className="slider_container">
-    <Swiper
-    spaceBetween={80}
-    slidesPerView={3}
-
-    onSlideChange={() => console.log('slide change')}
-    onSwiper={(swiper) => console.log(swiper)}
-    pagination={{
-      type: 'fraction',
-    }}
-    navigation={true}
-    virtual
-  >
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        pagination={{
+          type: "bullets",
+        }}
+        navigation={true}
+        virtual>
     <SwiperSlide><CardSuco/></SwiperSlide>
     <SwiperSlide><CardTee/></SwiperSlide>
     <SwiperSlide><CardEspresso/></SwiperSlide>
@@ -65,6 +36,7 @@ function Slider() {
 
     </div>
   );
-}
+      }
+
 
 export default Slider;
