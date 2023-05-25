961f2e7f6480a3febb07db3d638a061efe21c447
Dantas Beyer <dantasbeyer@gotmail.com>
2023-05-25 10:35:36 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import "./style/input.css";
-import Button from "./components/Button/Button";
-import ButtonAdm from "./components/Button/ButtonAdm";
-import Tag from "./components/Tag/Tag";
-import Stepper from "./components/Stepper/Stepper";
-import Myorders from "./components/MyOrders/Myorders.jsx";
 import Navbar from "./components/Navbar/Navbar";
-import NavbarAdm from "./components/Navbar/Navbar_adm";
 import Footer from "./components/Footer/Footer";
-import Cards from "./components/Cards/meals/CardParma";
-import CardsAdm from "./components/Cards/Card_adm";
 import Details from "./Pages/Details/Details";
 import Home from "./Pages/Home/Home";
 import AuthenticationPage from "./Pages/Authentication/AuthenticationPage";
-import SignIn from "./components/AuthenticationComp/SignIn";
-import SignUp from "./components/AuthenticationComp/SignUp";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
